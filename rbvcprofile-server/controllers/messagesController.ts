0e4077183516f51dbcc9d8cc854dcdb25462f879
rafaelbvc <rafaelbvc@hotmail.com>
2023-07-18 06:36:23 -0300

    v. 1.0.7 - Patch and Delete Messages controller

--- rbvcprofile-server/controllers/messagesController.ts
+++ rbvcprofile-server/controllers/messagesController.ts
@@ -7,9 +7,8 @@ const assyncHandler = require("express-async-handler");
 // get all messages - Get - Private
 export const getMessages = assyncHandler(async (req, res) => {
   const messages = await Messages.find().select().lean();
-  console.log(messages.length, "EUEUEEUEUEU")
   if (!messages?.length) {
-    return res.status(400).json({ message: "you have no messages yet..." })
+    return res.status(400).json({ message: "you have no messages yet..." });
   }
   res.json(messages);
 });
@@ -32,16 +31,57 @@ export const createMessage = assyncHandler(async (req, res) => {
     message,
   };
 
-  console.log(messageObject, "bcreatemessage");
-
   //create and store an Message
   const createNewMessage = await Messages.create(messageObject);
-
-  console.log(createNewMessage, "createmessage");
 
   if (createNewMessage) {
     res.status(201).json({ message: `The message ${title}, has been created` });
   } else {
     res.status(400).json({ message: "Invalid data" }); // todo error handling
   }
+});
+
+export const updateMessage = assyncHandler(async (req, res) => {
+  const { id, title, message } = req.body;
+
+  if (!id || !title || !message) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const messageUpdate = await Messages.findById(id).exec();
+
+  if (!messageUpdate) {
+    return res.status(400).json({ message: "Message dont exists" });
+  }
+
+  if (messageUpdate?._id.toString() !== id.toString()) {
+    return res.status(409).json({ message: "The id cant be validated" });
+  }
+
+  (messageUpdate.title = title), (messageUpdate.message = message);
+
+  const updateMessageId = await messageUpdate.save();
+
+  res.json({
+    message: `The message ${updateMessageId.title} has been updated!`,
+  });
+});
+
+export const deleteMessage = assyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ message: "The message id need to be given" });
+  }
+
+  const messageDelete = await Messages.findById(id.toString()).exec();
+  if (!messageDelete) {
+    res.status(400).json({ message: "Message not found" });
+  }
+
+  const result = await messageDelete.deleteOne();
+
+  const reply = `Message ${result.title} with id ${result._id} has been deleted`;
+
+  res.json(reply);
 });
--- rbvcprofile-server/routes/messagesRoutes.ts
+++ rbvcprofile-server/routes/messagesRoutes.ts
@@ -1,8 +1,8 @@
 import {Router} from "express"
-import { getMessages, createMessage } from "../controllers/messagesController"
+import { getMessages, createMessage, updateMessage, deleteMessage } from "../controllers/messagesController"
 const router = Router();
 
 
-router.route("/").get(getMessages).post(createMessage)
+router.route("/").get(getMessages).post(createMessage).patch(updateMessage).delete(deleteMessage)
 
 module.exports = router
