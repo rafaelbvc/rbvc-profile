4e79669ffed08c04a127c12d4f1fd8ba6c735f10
rafaelbvc <rafaelbvc@hotmail.com>
2023-08-09 05:55:55 -0300

    v. 0.3.2 - Stable

--- rbvcprofile-client/src/app/api/apiSlice.ts
+++ rbvcprofile-client/src/app/api/apiSlice.ts
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice: any = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5030" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://rbvcprofile-server.onrender.com/",
+  }),
   tagTypes: ["User", "Message"],
   endpoints: (builder: any) => ({}),
 });
